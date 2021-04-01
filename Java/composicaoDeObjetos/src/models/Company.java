e79240d8be37d1d4bedd997988527a96072ff0fe
jorgehauck <jorge.hauck1997@outlook.com>
2021-04-01 17:05:45 -0300

    Composição de Objetos, Herança, Polimorfismo, Injeção de depedência e exceções.

---
+++ Java/composicaoDeObjetos/src/models/Company.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Company extends TaxPayer {
+
+	private int numberOfEmployees;
+	
+	public  Company() {
+	}
+	
+	public Company(String name, Double anualIncome, int numberOfEmployees) {
+		super(name, anualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	@Override
+	public Double tax() {
+		Double imposto;
+		if(numberOfEmployees > 10) {
+			imposto = super.getAnualIncome() * 0.14;
+		}
+		else {
+			imposto = super.getAnualIncome() * 0.16;
+		}
+		return imposto;
+	}
+
+}
